c87ab9e13cf296ec244a5b773db21749e963abd7
Agaba Kabunga <agaba_k@hotmail.com>
2021-07-08 20:27:01 -0500

    Add project files.

---
+++ DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using IdentityAspNetCore.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityAspNetCore.DbContexts
+{
+    public class ApplicationDbContext: IdentityDbContext<AppUser>
+    {
+        public ApplicationDbContext(
+            DbContextOptions<ApplicationDbContext> options) :base(options)
+        { }
+    }
+}
---
+++ Models/AppUser.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+
+namespace IdentityAspNetCore.Models
+{
+    public class AppUser:IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime? DateOfBorn { get; set; }
+        public DateTime? LastDateLogin { get; set; }
+    }
+}
